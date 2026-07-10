57f21b1d6887e7b52b8bf5ff9795e2c47170d57e
FlyTripVisa <zs8j2n69sy@privaterelay.appleid.com>
2026-07-10 23:18:36 +0600

    Update App.tsx
    Deploy

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -8,7 +8,6 @@ function App() {
 	const [name, setName] = useState("Cloudflare");
 
 	return (
-		<>
 			<div>
 				<a href="https://vite.dev" target="_blank">
 					<img src={viteLogo} className="logo" alt="Vite logo" />
