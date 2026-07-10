006e13c23fdd90555816ce616604cb32b88cc9b5
FlyTripVisa <zs8j2n69sy@privaterelay.appleid.com>
2026-07-10 23:18:08 +0600

    Update App.tsx
    Deploy

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -5,7 +5,7 @@ import "./App.css";
 
 function App() {
 	const [count, setCount] = useState(0);
-	const [name, setName] = useState("cloud-file");
+	const [name, setName] = useState("Cloudflare");
 
 	return (
 		<>
