157b61dea615b52d49ee19b37bc47c9323a25dd5
FlyTripVisa <zs8j2n69sy@privaterelay.appleid.com>
2026-07-10 23:13:47 +0600

    Update App.tsx
    Deploy

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -17,19 +17,7 @@ function App() {
 				<a href="https://vite.dev" target="_blank">
 					<img src={viteLogo} className="logo" alt="Vite logo" />
 				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-				<a href="https://hono.dev/" target="_blank">
-					<img src={honoLogo} className="logo cloudflare" alt="Hono logo" />
-				</a>
-				<a href="https://workers.cloudflare.com/" target="_blank">
-					<img
-						src={cloudflareLogo}
-						className="logo cloudflare"
-						alt="Cloudflare logo"
-					/>
-				</a>
+				
 			</div>
 			<h1>cloudX</h1>
 			<div className="card">
